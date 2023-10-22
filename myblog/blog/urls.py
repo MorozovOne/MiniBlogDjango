57a481f931eef346cff7b0f394c7628d32ee6581
ZXCDmitry <abdulaevabu@mail.ru>
2023-10-22 15:58:20 +0300

    awdawdasd

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [path('', views.PostView.as_view(), name='Home'),
                path('login/', views.sign_in, name='login'),
                path('logout/', views.sign_out, name='logout'),
                path('register/', views.sign_up, name='register'),
-               path('profile/', views.Profile.as_view(), name='profile')
                ]
